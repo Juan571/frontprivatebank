0e93aca23a7d89d60a3737bbc4943bc72a2ffb41
Gleyder Cova <gleyder4@gmail.com>
2017-01-30 17:35:42 +0200

    Commit inicial, proyecto de reportes para BankInter

---
+++ js/config/routing.js
@@ -0,0 +1,37 @@
+/* Setup Rounting For All Pages */
+BankInterApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    // Redirect any unmatched url
+    $urlRouterProvider.otherwise("/dashboard.html");  
+    
+    $stateProvider
+
+        // Dashboard
+        .state('dashboard', {
+            url: "/dashboard.html",
+            templateUrl: "views/dashboard.html",            
+            data: {pageTitle: 'Extracto Mis Inversiones'},
+            controller: "DashboardController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BankInterApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+                        files: [
+                            'assets/global/plugins/morris/morris.css',                            
+                            'assets/global/plugins/morris/morris.min.js',
+                            'assets/global/plugins/morris/raphael-min.js',                            
+                            'assets/global/plugins/jquery.sparkline.min.js',
+
+                            'assets/pages/scripts/dashboard.min.js',
+                            'js/controllers/DashboardController.js',
+                        ] 
+                    });
+                }]
+            }
+        })
+
+
+   
+
+}]);
